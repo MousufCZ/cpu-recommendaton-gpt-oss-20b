21f3ddfc1419dec63b17e4254df9e5db818ae83e
Mousuf C Zaman <mousus.nayon@gmail.com>
2025-09-08 21:54:53 +0100

    New features added from existing columns.

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -4,35 +4,41 @@ import os
 from datetime import datetime
 
 # Paths
-RAW_CSV_PATH = "/Users/mousuf/ProgProj/oss-hackathon/OssCode/data/processed/cpu_benchmarks_v2_server_2025-09-08_17-34-24.csv"
+RAW_CSV_PATH = "/Users/mousuf/ProgProj/oss-hackathon/OssCode/data/processed/cpu_benchmarks_v3_cleaned_2025-09-08_20-52-44.csv"
 PROCESSED_DIR = "/Users/mousuf/ProgProj/oss-hackathon/OssCode/data/processed"
 os.makedirs(PROCESSED_DIR, exist_ok=True)
 
 def main():
     # Load CSV 
     df = pd.read_csv(RAW_CSV_PATH)
-    
-    # --- Convert test_date --- 
-    df['test_date'] = pd.to_datetime(df['test_date'], errors='coerce').dt.year
 
-    # --- Replace int/float to NaN and Sting/object to Unknown ---
-    # Define placeholder values to treat as missing
-    placeholders = ["", "N/A", "-", "unknown"]
+    # --- Current year for 'age' calculation ---
+    current_year = datetime.now().year
 
-    for col in df.columns:
-        if pd.api.types.is_numeric_dtype(df[col]):
-            df[col] = pd.to_numeric(df[col], errors='coerce')
-        elif pd.api.types.is_object_dtype(df[col]):
-            df[col] = df[col].replace(placeholders, "Unknown")
+    # --- Derived Features ---
+
+    # Cost-performance
+    df['price_per_core'] = df['price'] / df['cores']
+    df['thread_mark_per_dollar'] = df['thread_mark'] / df['price']
+
+    # Parallelism
+    df['thread_efficiency'] = df['thread_mark'] / df['cpu_mark']
+
+    # Efficiency / Thermals
+    df['threadMark_per_watt'] = df['thread_mark'] / df['TDP']
+    df['thermal_performance_ratio'] = df['power_performance'] / df['cpu_value']
+
+    # Architecture Evolution / Age
+    df['age'] = current_year - df['test_date']
 
     # Save back to the same file (overwrite)
     # df.to_csv(RAW_CSV_PATH, index=False)
 
-    # --- Save Version 3 (cleaned) ---
+    # # --- Save Versions (fearure engineering) ---
     timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    csv_v3_path = os.path.join(PROCESSED_DIR, f"cpu_benchmarks_v3_cleaned_{timestamp}.csv")
-    df.to_csv(csv_v3_path, index=False)
-    print(f"Version 3 (cleaned) saved to: {csv_v3_path}")
+    csv_version_path = os.path.join(PROCESSED_DIR, f"cpu_benchmarks_v4_feature_engineering_{timestamp}.csv")
+    df.to_csv(csv_version_path, index=False)
+    print(f"Version 4 (feature engineering) saved to: {csv_version_path}")
 
     print(f"============================")
     print(f"============================")
